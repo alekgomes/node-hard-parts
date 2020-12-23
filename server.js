ff54e9bfdf1b9b12a4b25e0bfd1e16a9296dfcc0
LucasAlek <lucas.alek@icarotech.com>
2020-12-23 16:22:51 -0300

    Estudo-Node: resolução exercícios

--- server.js
+++ server.js
@@ -15,17 +15,30 @@ function doOnRequest(request, response){
   }
   else if (request.method === 'POST' && request.url === '/sayHi') {
     // code here...
-    
+    fs.appendFileSync("hi_log.txt", "Somebody said hi.\n");
+    response.end("hi back to you!");
   }
   else if (request.method === 'POST' && request.url === '/greeting') {
     // accumulate the request body in a series of chunks
     // code here...
-    
+    let body = [];
+    request
+        .on("data", chunk => {
+            body.push(chunk);
+        })
+        .on("end", () => {
+            body = Buffer.concat(body).toString() + "\n";
+            fs.appendFileSync("hi_log.txt", body);
+            response.end(body);
+        });
   }
   else {
     // Handle 404 error: page not found
     // code here...
-    
+    response.statusCode = 404;
+    response.statusMessage = "Error: Not Found.";
+    response.end();
+
   }
 }
 
